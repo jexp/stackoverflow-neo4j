07c09d0707df85566c9ce6ad18391bd489a45ca2
Michael Hunger <github@jexp.de>
2015-08-27 17:23:04 +0200

    Update import.sh

--- import.sh
+++ import.sh
@@ -1,11 +1,17 @@
-rm -rf ../neo/data/graph.db
-../neo/bin/neo4j-import \
---into ../neo/data/graph.db \
+NEO=${1-../neo}
+DB=${2-$NEO/data/graph.db}
+echo NEO $NEO DB $DB
+rm -rf $DB
+$NEO/bin/neo4j-import \
+--bad-tolerance 10000 \
+--skip-bad-relationships \
+--into $DB \
 --id-type string \
---nodes:Post csvs/posts.csv \
---nodes:User csvs/users.csv \
---nodes:Tag csvs/tags.csv \
---relationships:PARENT_OF csvs/posts_rel.csv \
---relationships:HAS_TAG csvs/tags_posts_rel.csv \
---relationships:POSTED csvs/users_posts_rel.csv
-../neo/bin/neo4j restart
+--nodes:Post csvs/posts.csv.gz \
+--nodes:User csvs/users.csv.gz \
+--nodes:Tag csvs/tags.csv.gz \
+--relationships:PARENT_OF csvs/posts_rel.csv.gz \
+--relationships:ANSWER csvs/posts_answers.csv.gz \
+--relationships:HAS_TAG csvs/tags_posts_rel.csv.gz \
+--relationships:POSTED csvs/users_posts_rel.csv.gz
+#$NEO/bin/neo4j restart
